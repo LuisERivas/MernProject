5c4680bf9c8e6b0b4fd55d8f20d28a80553d25a9
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-06-24 11:21:38 -0700

    created basic connection of atlas with mongoose

---
+++ config/db.js
@@ -0,0 +1,27 @@
+// bring in dependecies
+// used to have mongo interact with our sever
+const mongoose = require('mongoose')
+// to access the global settings we set up in the config file
+const config = require('config')
+// to access a specific variable form the config file
+const db = config.get('mongoURI')
+
+// to connect to mongoDB
+const connectDB = async () => {
+  // try to connect to server
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log('MongoDB Connected to sever bro')
+    // if it cant connect to the server
+  } catch (err) {
+    console.error(err.message)
+    // Exit process if it fails
+    process.exit(1)
+  }
+}
+
+// export
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Dependencies
 // brings in express
 const express = require('express')
+// bring in server
+const connectDB = require('./config/db')
 
 // app uses express
 const app = express()
+// connect databasee
+connectDB()
 
 // server end point (for testing right now)
-app.get('/', (req, res)=> res.send('Api up and running'))
+app.get('/', (req, res) => res.send('Api up and running'))
 
 // set up port for server
 // if cant find porn, it will default to 5000
-const PORT =  process.env.Port || 5000
+const PORT = process.env.Port || 5000
 
 // look for server port
-app.listen (PORT, () => console.log(`Server started on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
